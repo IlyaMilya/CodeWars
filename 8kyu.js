9e127526e1b43675943c6d0a7a2006607da00c0c
Ilya <milyavskyfamily@gmail.com>
2022-11-30 23:56:38 -0500

    One more nightly challenge before bed

--- 8kyu.js
+++ 8kyu.js
@@ -28,3 +28,57 @@ else array.forEach(function(element){
   average = amount/array.length
  return average
   }
+
+//   Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+
+
+
+
+
+// // Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+// if bmi <= 25.0 return "Normal"
+// if bmi <= 30.0 return "Overweight"
+// if bmi > 30 return "Obese"
+
+function opposite(number) {
+    var result = number * -1
+     return result
+   }
+
+   function bmi(weight, height) {
+    let health = weight/height**2
+   
+   if (health <= 18.5){
+     return "Underweight"
+   }
+   else if (health <= 25.0) {
+    return "Normal"
+   }
+   if (health <= 30.0){
+     return "Overweight"
+   }
+   else if (health > 30){
+     return "Obese"
+   }
+   }
+   
+ //   switch(health) {
+ //       case bmi <= 18.5:
+ //      return "Underweight";
+ //       case bmi <= 25.0:
+ //       return "Normal";
+ //       case bmi <= 30.0:
+ //       return "Overweight";
+ //       case bmi > 30:
+ //      return "Obese";
+ //     }
+ //   }
