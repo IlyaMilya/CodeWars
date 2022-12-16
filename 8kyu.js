3f95fbda46027faa9eda7c05afdac2d50e215100
Ilya <milyavskyfamily@gmail.com>
2022-12-16 00:18:31 -0500

    invert arrays algorithm

--- 8kyu.js
+++ 8kyu.js
@@ -82,3 +82,22 @@ function opposite(number) {
  //      return "Obese";
  //     }
  //   }
+
+
+
+
+
+ //Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+ function invert(array) {
+    let newArray= []
+    array.map((e)=> {
+     let y = e * -1
+     newArray.push(y)})
+      return newArray
+    };
