5927664a63dc492b3388b08ed82d12301f891a90
Ilya <milyavskyfamily@gmail.com>
2023-01-10 23:43:57 -0500

    finished grade calculation <=18 algorithm

--- 7kyu.js
+++ 7kyu.js
@@ -46,20 +46,13 @@ function sum(numArray){
     let passedStudents = 0
     let scoreAve = 0 
     let rounded = 0
-  //   function filterScore(value) {
-  //   return value <= 18; 
-  // }
-  //   let filtered = list.filter(filterScore)
-  // list.forEach(function(e){if (e > 18){
-  //     list.pop(e)
-  //     list.pop(e)
-  //   }
-  //   })
-   list.forEach(function(element)
+    const passed = list.filter(element => element <= 18)
+    console.log(passed)
+   passed.forEach(function(element)
     {passedStudents += element})
-    scoreAve = passedStudents/list.length
+    scoreAve = passedStudents/passed.length
     rounded = Math.round(scoreAve)
-    if (rounded > 18) 
+    if (rounded > 18 || passed.length === 0) 
     {return 'No pass scores registered.'}
   else return rounded
   }
