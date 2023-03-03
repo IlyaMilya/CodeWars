bf933b3031d4a7c6e23495e575009871e4a01540
Ilya <milyavskyfamily@gmail.com>
2023-03-03 16:04:23 -0500

    solved greetname algo

--- 7kyu.js
+++ 7kyu.js
@@ -61,5 +61,20 @@ function sum(numArray){
 
 
 
+  // Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+  // Example:
+  
+  // "riley" --> "Hello Riley!"
+  // "JACK"  --> "Hello Jack!"
+
+  var greet = function(name) {
+    let named = name.toLowerCase()
+    let index = name.length
+  return (`Hello ${name[0].toUpperCase()+named.slice(1, index )}!`)
+  };
+
+
+
 
   
